d9b799bc1f7ad2a43f6a012b055213e96b3dfd6e
zhangshibo <zhangshibo@cnic.cn>
2018-05-02 14:31:15 +0800

    新建测试类

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ test
 ====
 
 test project
+
+本项目作为本人测试学习之用。
---
+++ src/main/java/cn/ddx/lc/test/Test.java
@@ -0,0 +1,14 @@
+package cn.ddx.lc.test;
+/**
+ * 
+ * @author zhang
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
